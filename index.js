330bcfbe3605de8873aa826a69f97cf4865ac577
Cody Hale <hale.l.c91@gmail.com>
2019-04-24 01:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return "Happy holidays!"
 }
+
 function happyHolidaysto(name) {
   var name = Shianna
-  return "Happy holidays, + $(name)!"
+  return "Happy holidays, $(name)!"
 }
