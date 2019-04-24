96873e3a53ae9ce0a2dd4e08a3f6114ea1ca56eb
Cody Hale <hale.l.c91@gmail.com>
2019-04-24 01:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
+
+function happyHolidaysTo (holiday, name) {
+  return "happy ${holiday}, ${name}!"
+}
+
+function happyHolidaysTo(holiday, days) {
+  return "It\'s $(days) days until $(holiday)!"
+}
